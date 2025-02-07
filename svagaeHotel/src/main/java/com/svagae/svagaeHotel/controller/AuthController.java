bf25ad2f716571560e7284855c7ccded45061c70
Mohamed <145871589+svagae@users.noreply.github.com>
2025-02-07 21:31:19 +0200

    AuthController

---
+++ svagaeHotel/src/main/java/com/svagae/svagaeHotel/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.svagae.svagaeHotel.controller;
+
+
+import com.svagae.svagaeHotel.dto.Response;
+import com.svagae.svagaeHotel.entity.User;
+import com.svagae.svagaeHotel.service.interfac.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
